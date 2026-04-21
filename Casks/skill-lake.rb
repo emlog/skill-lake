f7ee61d4ff1d32964fbff9294af5b747546695d3
snow <4344235+emlog@users.noreply.github.com>
2026-04-21 09:28:32 +0800

    chore: release 1.1.5

--- Casks/skill-lake.rb
+++ Casks/skill-lake.rb
@@ -1,8 +1,8 @@
 cask "skill-lake" do
-  version "1.1.4"
-  sha256 "2fa2d4f3b462a2fc76e620b144b130c3e697b353ccbd92423e8b245060c57795"
+  version "1.1.5"
+  sha256 "2706e19c95a88a0796d0e7f284d775b20283b6a31f041cee54c36a982b4a7726"
 
-  url "https://github.com/emlog/skill-lake/releases/download/1.1.4/SkillLake-1.1.4.dmg"
+  url "https://github.com/emlog/skill-lake/releases/download/1.1.5/SkillLake-1.1.5.dmg"
   name "Skill Lake"
   desc "A local AI agent skill manager app for macOS"
   homepage "https://github.com/emlog/skill-lake"
